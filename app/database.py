d6d558b47a8be026427c0023eadf711a4c6f8082
jforbes02 <131290572+jforbes02@users.noreply.github.com>
2025-08-08 01:54:15 -0400

    initial commit

---
+++ app/database.py
@@ -0,0 +1,25 @@
+#connecting to postgresql
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+#Database connection url
+DATABASE_URL = "postgresql://postgres:Coolpyro55@localhost:5432/clubbies_db"
+
+# creates database engine(translates python->sql)
+engine = create_engine(DATABASE_URL)
+
+#creates sessions for changing the database per each API request
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#Parent class
+Base = DeclarativeBase()
+
+
+#gets database session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/models.py
@@ -0,0 +1,6 @@
+#what data is accepted and what do my tables look like
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from pydantic import BaseModel
+from database import Base
+
+class User
