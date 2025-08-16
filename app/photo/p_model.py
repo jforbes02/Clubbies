8d1a37a1c95dd5bdb956f396ab83bdbf7341b20d
forbesCoding <131290572+jforbes02@users.noreply.github.com>
2025-08-15 21:51:56 -0400

    Start of photos

---
+++ app/photo/p_model.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+
+class PhotoBase(BaseModel):
+    caption: Optional[str] = Field(None, max_length=255)
+    venue_id: int = Field(..., gt=0)
+
+class PhotoResponse(BaseModel):
+    photo_id: int
+    img_url: str = Field(..., description="URL to access uploaded img")
+    file_size: Optional[int] = Field(None, description="File size in bytes")
+    venue_id: int = Field(..., gt=0)
+    venue_name: str = Field(..., max_length=255)
+    user_id: int
+    username: str
+    uploaded_at: datetime
+    caption: Optional[str] = Field(None, max_length=255)
+
+    class Config:
+        from_attributes = True
+class PhotoUpdate(BaseModel):
+    caption: Optional[str] = Field(None, max_length=255)
